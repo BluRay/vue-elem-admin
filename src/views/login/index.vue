73e55f265a738aad50ce6fbec86f7726e853ebc2
yangke <yangke0227@gmail.com>
2022-06-05 20:09:31 +0800

    ++

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -45,7 +45,7 @@
         </el-form-item>
       </el-tooltip>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登陆</el-button>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
     </el-form>
 
     <el-dialog title="Or connect with" :visible.sync="showDialog">
