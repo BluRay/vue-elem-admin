1d4df24ee2549967ba36a7ad1214d8b04985f57e
yangke0227@gmail.com <yangke0227@gmail.com>
2025-10-20 22:42:03 +0800

    ++交易系统配置功能

--- src/views/documentation/trade_books.vue
+++ src/views/documentation/trade_books.vue
@@ -15,8 +15,8 @@
           </template>
         </vxe-form-item>
         <vxe-form-item>
-          <vxe-button size="mini" status="success" @click="fetchData()">查询</vxe-button>
-          <vxe-button size="mini" status="warning" @click="showModel = true">新增</vxe-button>
+          <vxe-button size="mini" status="primary" @click="fetchData()">查询</vxe-button>
+          <vxe-button size="mini" status="success" @click="showModel = true">新增</vxe-button>
         </vxe-form-item>
       </vxe-form> &nbsp;&nbsp;
     </el-header>
@@ -38,9 +38,37 @@
         <vxe-table-column field="exchange_name" title="交易所" width="100px" />
         <vxe-table-column field="status" title="状态" width="90px" />
         <vxe-table-column field="memo" title="备注" width="90px" />
-        <vxe-table-column field="memo" title="操作" width="90px" />
+        <vxe-table-column title="操作" width="200">
+          <template #default="{ row }">
+            <vxe-button status="warning" content="编辑" @click="updateRowEvent(row)"></vxe-button>
+            <vxe-button status="danger" content="删除" @click="deleteRowEvent(row)"></vxe-button>
+          </template>
+        </vxe-table-column>
       </vxe-table>
     </el-main>
+    <vxe-modal v-model="showModel" title="新增交易品种" size="mini" width="500" show-footer>
+      <template #default>
+      <vxe-form refs="add_form">
+        <vxe-form-item field="flag1" title="合约编号:" title-width="70px" :span="12" :item-render="{}">
+          <vxe-input v-model="book.book_id" placeholder="请输入合约编号"></vxe-input>
+        </vxe-form-item>
+        <vxe-form-item field="flag1" title="交易品种:" title-width="70px" :span="12" :item-render="{}">
+          <vxe-input v-model="book.book_name" placeholder="请输入交易品种"></vxe-input>
+        </vxe-form-item>
+        <vxe-form-item field="flag1" title="交 易 所:" title-width="70px" :span="12" :item-render="{}">
+          <vxe-select v-model="book.exchange_id" transfer>
+            <vxe-option v-for="item in exchangeList" :key="item.value" :value="item.value" :label="item.label"></vxe-option>
+          </vxe-select>
+        </vxe-form-item>
+        <vxe-form-item align="center" title-align="left" :span="24">
+          <template #default>
+            <vxe-button type="submit">提交</vxe-button>
+            <vxe-button type="reset">重置</vxe-button>
+          </template>
+        </vxe-form-item>
+      </vxe-form>
+      </template>
+    </vxe-modal>
   </el-container>
 </template>
 
@@ -56,7 +84,9 @@ export default {
       loading: false,
       tableheight: '500px',
 	    pageSizes: [100, 500, 1000, 5000],
-      tableData: []
+      tableData: [],
+      exchangeList: [{label:'上期所',value:'AAA'},{label:'bbb',value:'bbb'}],
+      book: {}
     }
   },
   created() {
@@ -72,6 +102,12 @@ export default {
         this.tableData = response.data.result.data.dateList
         this.loading = false
       })
+    },
+    deleteRowEvent(row) {
+	    if(confirm("确认要删除吗?")) {
+	    	this.$message({ message: '删除成功!', type: 'success' })
+	    }
+
     },
     exportDataEvent() {
       this.$refs.xTable.exportData({
--- src/views/documentation/trade_main.vue
+++ src/views/documentation/trade_main.vue
@@ -2,21 +2,16 @@
   <el-container>
     <el-header>
       <vxe-form refs="grid_form">
-        <vxe-form-item title="关键字:" title-width="80px">
+        <vxe-form-item title="交易帐号:" title-width="80px">
           <template #default>
-            <vxe-input
-              v-model="searchForm.keyword"
-              clearable
-              size="mini"
-              placeholder="请输入交易账户/名称/资金账户"
-              style="width: 250px;"
-              class="filter-item"
-            />
+            <vxe-select v-model="searchForm.exchange_id" transfer>
+            <vxe-option v-for="item in accountList" :key="item.value" :value="item.value" :label="item.label"></vxe-option>
+          </vxe-select>
           </template>
         </vxe-form-item>
         <vxe-form-item>
-          <vxe-button size="mini" status="success" @click="fetchData()">查询</vxe-button>
-          <vxe-button size="mini" status="warning" @click="showModel = true">新增</vxe-button>
+          <vxe-button size="mini" status="success" @click="fetchData()">帐户信息</vxe-button>
+          <vxe-button size="mini" status="warning" @click="showModel = true">成交记录</vxe-button>
         </vxe-form-item>
       </vxe-form> &nbsp;&nbsp;
     </el-header>
@@ -35,10 +30,29 @@
         <vxe-table-column type="seq" title="序号" fixed="left" width="45px" />
         <vxe-table-column field="book_id" title="合约编号" fixed="left" width="100px" />
         <vxe-table-column field="book_name" title="交易品种" fixed="left" width="100px" />
-        <vxe-table-column field="exchange_name" title="交易所" width="100px" />
-        <vxe-table-column field="status" title="状态" width="90px" />
-        <vxe-table-column field="memo" title="备注" width="90px" />
-        <vxe-table-column field="memo" title="操作" width="90px" />
+        <vxe-table-column field="exchange_name" title="交易所" width="90px" />
+        <vxe-table-column field="status" title="最新价格" width="90px" />
+        <vxe-table-column field="status" title="涨跌幅" width="90px" />
+        <vxe-table-column field="status" title="总持仓" width="80px" />
+        <vxe-table-column field="status" title="今持仓" width="80px" />
+        <vxe-table-column field="memo" title="委托单" width="80px" />
+        <vxe-table-column field="memo" title="持仓盈亏" width="80px" />
+        <vxe-table-column field="memo" title="平仓盈亏" width="80px" />
+        <vxe-table-column field="memo" title="手续费" width="80px" />
+        <vxe-table-column title="交易手数" width="80px" fixed="right">
+          <template #default="{ row }">
+            <vxe-input type="number" value='1'></vxe-input>
+          </template>
+        </vxe-table-column>
+        <vxe-table-column title="交易" width="320" fixed="right">
+          <template #default="{ row }">
+            <vxe-button status="warning" content="开多" ></vxe-button>
+            <vxe-button status="danger" content="平多" ></vxe-button>
+            <vxe-button status="warning" content="开空" ></vxe-button>
+            <vxe-button status="danger" content="平空" ></vxe-button>
+            <vxe-button status="success" content="委托" ></vxe-button>
+          </template>
+        </vxe-table-column>
       </vxe-table>
     </el-main>
   </el-container>
@@ -56,7 +70,8 @@ export default {
       loading: false,
       tableheight: '500px',
 	    pageSizes: [100, 500, 1000, 5000],
-      tableData: []
+      tableData: [],
+      accountList: []
     }
   },
   created() {
