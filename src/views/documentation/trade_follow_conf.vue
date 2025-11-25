d58d58d3bbd1d80756ded21b71a96e721b56d360
yangke0227@gmail.com <yangke0227@gmail.com>
2025-11-25 23:03:31 +0800

    ++RuiqiData

--- src/views/documentation/trade_follow_conf.vue
+++ src/views/documentation/trade_follow_conf.vue
@@ -8,14 +8,15 @@
               v-model="searchForm.keyword"
               clearable
               size="mini"
-              placeholder="请输入交易账户/名称/资金账户"
+              placeholder="请输入交易账户/姓名/联系电话"
               style="width: 250px;"
               class="filter-item"
             />
           </template>
         </vxe-form-item>
         <vxe-form-item>
-          <vxe-button size="mini" status="success" @click="fetchData()">查询交易账号</vxe-button>
+          <vxe-button size="mini" status="primary" @click="fetchData()">查询跟单账号</vxe-button>
+          <vxe-button size="mini" status="success" @click="addFollerUser()">新增跟单账号</vxe-button>
         </vxe-form-item>
       </vxe-form> &nbsp;&nbsp;
     </el-header>
@@ -33,10 +34,11 @@
       >
         <vxe-table-column type="seq" title="序号" fixed="left" width="45px" />
         <vxe-table-column field="username" title="跟单账户" fixed="left" width="95px" sortable />
-        <vxe-table-column field="display_name" title="账户名称" width="95px" />
+        <vxe-table-column field="display_name" title="账户姓名" width="95px" />
         <vxe-table-column field="trade_account" title="交易帐号" width="95px" />
-        <vxe-table-column field="user_type" title="帐号类型" width="95px" />
-        <vxe-table-column field="user_manager" title="管理员" width="95px" />
+        <vxe-table-column field="trade_company" title="期货公司" width="95px" />
+        <vxe-table-column field="phone_number" title="联系电话" width="95px" />
+        <vxe-table-column field="address" title="联系地址" width="95px" />
         <vxe-table-column field="status" title="状态" width="95px" />
         <vxe-table-column field="follow_tactics_1" title="跟单策略1" width="95px" />
         <vxe-table-column field="follow_tactics_2" title="跟单策略2" width="95px" />
@@ -44,7 +46,7 @@
         <vxe-table-column field="follow_tactics_4" title="跟单策略4" width="95px" />
         <vxe-table-column field="follow_tactics_5" title="跟单策略5" width="95px" />
         <vxe-table-column field="memo" title="备注" width="125px" />
-        <vxe-table-column title="操作" width="200">
+        <vxe-table-column title="操作" fixed="right" width="150">
           <template #default="{ row }">
             <vxe-button status="warning" content="编辑" @click="updateRowEvent(row)" />
             <vxe-button status="danger" content="删除" @click="deleteRowEvent(row)" />
