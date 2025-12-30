bfcdfed7a29512d2e491ae1e36558b7522dc5489
yangke0227@gmail.com <yangke0227@gmail.com>
2025-12-30 21:37:17 +0800

    FollowTactics@MBP

--- src/views/documentation/trade_follow.vue
+++ src/views/documentation/trade_follow.vue
@@ -74,6 +74,7 @@ export default {
       following: false,
       tableheight: '500px',
       follow: [],
+      f_wt_list: [],
       tableData: [{f_status: '0'}, {f_status: '1'}],
       pageSizes: [100, 500, 1000, 5000]
     }
@@ -105,6 +106,103 @@ export default {
   		console.log('-->main_loop:', this.follow)
       this.following = true
       setTimeout(() => {this.following = false}, 5000) // 单次循环最大等待时间
+      let start_time = this.addTimeToDate(this.formatDate(new Date()), 0, 0, -10).substring(11)
+      let today_date = this.formatDate(new Date()).substring(0, 10).replaceAll('-', '')
+      console.log(today_date + '|' + start_time)
+      const _this = this
+      this.follow.forEach(f => {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.open('GET', this.main_url + '/get_today_order?account=' + f.tactics_account , true)
+        httpRequest.timeout = 2000;
+        httpRequest.send();
+        httpRequest.onreadystatechange = function () {
+          if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            if (httpRequest.responseText === '未获得订单数据') {
+              return false
+            }
+            var json = JSON.parse(httpRequest.responseText)
+            console.log(json)
+            for (var c in json) {
+              json[c].forEach(item => {
+                // OrderStatus 3: 委托; 5: 已撤; 0: 成交
+                if (item.OrderStatus === '3' && item.InsertDate === today_date && item.InsertTime > start_time) {
+                  let OrderSysID = item.OrderSysID
+                  if (_this.f_wt_list.indexOf(OrderSysID) < 0) {
+                    _this.f_wt_list.push(OrderSysID)
+                  }
+                }
+                // 判断委托单是否成交
+                let check_wt = false
+                if (item.OrderStatus === '0' && _this.f_wt_list.indexOf(item.OrderSysID) >= 0 && item.InsertDate === today_date) {
+                  _this.f_wt_list.splice(_this.f_wt_list.indexOf(item.OrderSysID), 1)
+                  check_wt = true
+                }
+                if (check_wt || (item.OrderStatus === '0' && item.InsertDate === today_date && item.InsertTime > start_time)) {
+                  // 过滤当前时间10秒以前的订单 过滤未成交的订单 过滤已跟的订单 InsertTime格式 09:15:32
+                  // 根据策略规则跟单
+                  let OrderSysID = item.OrderSysID          // 报单编号
+                  let InstrumentID = item.InstrumentID      // 合约代码
+                  let Direction = item.Direction            // 买卖方向 （'0' 买，'1' 卖）
+                  let CombOffsetFlag = item.CombOffsetFlag  // 组合开平标志 （'0' 开仓，'1' 平仓，'3' 平今等）
+                  let VolumeTraded = item.VolumeTraded * parseInt(tactics.f_tactics_multiplier || '1')      // 成交数量
+                  var httpRequest2 = new XMLHttpRequest();  // 跟单交易请求【TODO】正式使用需调客户本地交易服务地址
+                  let trade_str = (Direction === '0') ? 'buy' : 'sell'
+                  if ((tactics.f_tactics_way || '正向') === '反向') {
+                    trade_str = ((trade_str === 'buy') ? 'sell' : 'buy')
+                  }
+                  trade_str += (CombOffsetFlag === '0') ? 'open' : 'close'
+                  if (_this.followed_order_list.indexOf(OrderSysID) < 0) {
+                    console.log('-->[Followed]' + _this.real_main_url + '/' + trade_str + '?account=' + _this.account + '&bookid=' + InstrumentID + '&quantity=' + VolumeTraded)
+                    _this.followed_order_list.push(OrderSysID)
+                    _this.trade_logs.push({id: _this.logindex, logs: _this.c_time + '[自动]' + trade_str + '[' + InstrumentID + '] ' + VolumeTraded + '手' })
+                    _this.logindex++
+                    if (CombOffsetFlag !== '0') { // 平仓时 判断当前仓位是否小于平仓数量
+                      let cur_cc = 0              // 当前仓位
+                      if (trade_str === 'buyclose') {    // buyclose (Direction === '0')
+                        _this.cc_list.forEach(cc => {
+                          if (cc.InstrumentID === InstrumentID && cc.PosiDirection === '3') {
+                            cur_cc = cc.Position
+                          }
+                        })
+                      } else { // sellclose
+                        _this.cc_list.forEach(cc => {
+                          if (cc.InstrumentID === InstrumentID && cc.PosiDirection === '2') {
+                            cur_cc = cc.Position
+                          }
+                        })
+                      }
+                      console.log('平仓时 判断当前仓位是否小于平仓数量 cur_cc:' + cur_cc + ',VolumeTraded=' + VolumeTraded)
+                      if (cur_cc < VolumeTraded) {
+                        VolumeTraded = cur_cc
+                      }
+                    }
+
+                    httpRequest2.open('GET', _this.real_main_url + '/' + trade_str + '?account=' + _this.account + '&bookid=' + InstrumentID + '&quantity=' + VolumeTraded + '&tactics=f_' + tactics.f_tactics_id , true)
+                    httpRequest2.timeout = 5000
+                    httpRequest2.send()
+                    httpRequest2.onreadystatechange = function () {
+                      if (httpRequest2.readyState == 4) {
+                        if (httpRequest2.status == 200) {
+                          console.log(httpRequest2.responseText)
+                          if (httpRequest2.responseText === 'success:0'){
+                            _this.$notify({ title: '提示', message: trade_str + ' 交易成功!', duration: 1500 })
+                          } else {
+                            _this.$notify({ title: '提示', message: trade_str + ' 交易失败!!', duration: 1500 })
+                          }
+                        } else {
+                          _this.$notify({ title: '提示', message: trade_str + ' 交易失败!', duration: 1500 })
+                        }
+                      }
+                    }
+                  }
+                }
+              })
+            }
+          }
+        }
+
+      })
+      // this.following = false
       console.log('-->main_loop over')
   	},
     batch_start() {
@@ -157,6 +255,22 @@ export default {
       } else {
         return ''
       }
+    },
+    formatDate(date) {
+      const year = date.getFullYear()
+      const month = String(date.getMonth() + 1).padStart(2, '0')
+      const day = String(date.getDate()).padStart(2, '0')
+      const hours = String(date.getHours()).padStart(2, '0')
+      const minutes = String(date.getMinutes()).padStart(2, '0')
+      const seconds = String(date.getSeconds()).padStart(2, '0')
+      return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+    },
+    addTimeToDate(dateString, hours = 0, minutes = 0, seconds = 0) {
+      const date = new Date(dateString);
+      date.setHours(date.getHours() + hours)
+      date.setMinutes(date.getMinutes() + minutes)
+      date.setSeconds(date.getSeconds() + seconds)
+      return this.formatDate(date)
     }
   }
 }
